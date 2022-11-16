974f080b0c5eeaafb00225a8e2c2d4c73cb39d79
Saverio <sperrone1@studenti.uninsubria.it>
2022-11-16 18:27:12 +0100

    aggiustamenti vari

--- js/webgazer.js
+++ js/webgazer.js
@@ -5,13 +5,15 @@ window.saveDataAcrossSessions = false
 
 //this is used to tell the html page that there are a lot of data to read and that this process is fast. This will optimize the gathering of the data
 const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d', {willReadFrequently: true});
+const ctx = canvas.getContext('2d', { willReadFrequently: true });
 
 var start_record = false
 var i = 0
 var data_array = []
 
-/* webgazer.setGazeListener((data, timestamp) => {
+webgazer.setRegression("ridge")
+
+webgazer.setGazeListener((data, timestamp) => {
 
     //elapsed time is based on time since begin was called
     //setInterval(console.log(data, "timestamp is ", timestamp), 20)
@@ -27,27 +29,28 @@ var data_array = []
         setTimeout(100)
     }
 
-    }).begin() */
+}).begin()
 
-
-    webgazer.setGazeListener(function(data, elapsedTime) {
-        if (data == null) {
-            return;
-        }
-        var xprediction = data.x; //these x coordinates are relative to the viewport
-        var yprediction = data.y; //these y coordinates are relative to the viewport
-        //console.log(elapsedTime); //elapsed time is based on time since begin was called
-    }).begin();
+/*
+webgazer.setGazeListener(function (data, elapsedTime) {
+    if (data == null) {
+        return;
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    //console.log(elapsedTime); //elapsed time is based on time since begin was called
+}).begin();
+*/
 
 
 //it's used to stop the tracker
-pause = function() {
+pause = function () {
     webgazer.pause()
     console.log("Pause")
 }
 
 //it's used to resume the tracker
-resume = function() {
+resume = function () {
     webgazer.resume()
     console.log("Resume")
 }
@@ -55,58 +58,72 @@ resume = function() {
 var index_image = 0
 let img_array = ["Griglia.png", "text.png", "task.png", "dv1.png", "dv2.png", "dv3.png", "dv4.png"]
 
-previous = function() {
+previous = function () {
     if (index_image > 0) {
         index_image--
         document.getElementById("img").src = "..\\img\\" + img_array[index_image]
     }
 }
 
-next = function() {
+next = function () {
     if (index_image < 6) {
         index_image++
         document.getElementById("img").src = "..\\img\\" + img_array[index_image]
     }
 }
 
-//used to start and stop the acquisition of the data. When you stop it, it will save in a csv file all the data gathered before and download it
-start = function() {
-    if (start_record === false) {
-        start_record = true
-    } else {
-        start_record = false
-        let csvContent = "x,y,timestamp" + "\r\n"
-        data_array.forEach((element) => {
-            console.log(element)
-            let row = element.x + "," + element.y + "," +element.time
-            csvContent += row + "\r\n"
-        })
-        data_array = []
-
-        var hiddenElement = document.createElement('a');  
-        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvContent);  
-        hiddenElement.target = '_blank';  
-          
-        //provide the name for the CSV file to be downloaded  
-        hiddenElement.download = 'GazeData.csv';  
-        hiddenElement.click();  
-
-        /*
-        var encodedUri = encodeURI(csvContent)
-        window.open(encodedUri)
-        
-        var encodedUri = encodeURI(csvContent);
-        var link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "dati.csv");
-        document.body.appendChild(link); 
-        link.click();
-        */
-    }
-    console.log("Premuto tasto", start_record)
+//used to start and stop the acquisition of the data. 
+start = function () {
+    start_record = !start_record
 }
 
-//unused for now
-chart = function() {
-    
+var file_name = ""
+
+//When typing thename and clicking 'Enter', the system will save in a csv file all the data gathered before and download it
+window.onload = function () {
+    var input = document.getElementById("textInput")
+
+    input.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+
+            file_name = document.getElementById('textInput').value + ".csv"
+
+            let csvContent = "x,y,timestamp" + "\r\n"
+            data_array.forEach((element) => {
+                console.log(element)
+                let row = element.x + "," + element.y + "," + element.time
+                csvContent += row + "\r\n"
+            })
+            data_array = []
+
+            var hiddenElement = document.createElement('a');
+            hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvContent);
+            hiddenElement.target = '_blank';
+
+            //provide the name for the CSV file to be downloaded  
+            hiddenElement.download = file_name;
+            hiddenElement.click();
+
+            document.getElementById('textInput').value = ''
+
+            /*
+            var encodedUri = encodeURI(csvContent)
+            window.open(encodedUri)
+            
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "dati.csv");
+            document.body.appendChild(link); 
+            link.click();
+            */
+        }
+    })
+}
+
+
+//used to get the name of the file csv
+getName = function (input) {
+
+    input.value = file_name.toUpperCase()
 }
--- py/data_analysis.py
+++ py/data_analysis.py
@@ -8,10 +8,10 @@ from pathlib import Path as ph
 from matplotlib.patches import Circle
 from matplotlib.pyplot import figure, plot, scatter, show, text, subplots, imread, hist2d
 
-#this part is used to move the download file from the browser into the csv folder of the project 
-#from the download folder of the PC. The library used is the best for this task because it 
-#automatically create the specific path based on the system used (Windows, MacOs), so less 
-#job to do
+# this part is used to move the download file from the browser into the csv folder of the project
+# from the download folder of the PC. The library used is the best for this task because it
+# automatically create the specific path based on the system used (Windows, MacOs), so less
+# job to do
 data_source = ph.home() / 'Downloads' / 'GazeData.csv'
 data_destination = ph.home() / 'Desktop' / 'WebGazer' / 'csv'
 data_exists = data_destination / 'GazeData.csv'
@@ -25,99 +25,101 @@ else:
     sh.move(data_source, data_destination, copy_function=sh.copy)
 '''
 
-#from now, the file with the data is open and it is possible to work with it. I used the 'with open'
-#because it closes by default the file. That's helpful in case you forget to call the function .close()
-with open("csv/GazeData.csv") as file:
+# from now, the file with the data is open and it is possible to work with it. I used the 'with open'
+# because it closes by default the file. That's helpful in case you forget to call the function .close()
+with open("csv/TestTesto.csv") as file:
 
-    #read the csv file
-    reader_obj = csv.reader(file) 
+    # read the csv file
+    reader_obj = csv.reader(file)
 
-    #create a list to insert the data from the raw data
+    # create a list to insert the data from the raw data
     list_data = list()
     for row in reader_obj:
         list_data.append(row)
 
-    #pop the first two line which do not contain useful data (for example the name of the columns)
+    # pop the first two line which do not contain useful data (for example the name of the columns)
     list_data.pop(0)
     list_data.pop(0)
 
-    #create a list which contains the same data as before but casted to float type in order to
-    #use them to create the fixation points in the chart. This list contains tuples made of x coords, 
-    # y coords and timestamp. 
+    # create a list which contains the same data as before but casted to float type in order to
+    # use them to create the fixation points in the chart. This list contains tuples made of x coords,
+    # y coords and timestamp.
     list_circles = list()
-    for [a,b,c] in list_data:
+    for [a, b, c] in list_data:
         list_circles.append([float(a), (float(b)), float(c)])
 
-
-    #it's used to create a list that contains the distance of each pair of points and the time 
-    #difference between them to try to create fixation points and saccades. DMAX is the maximum 
-    #space distance between two points to consider them a fixation; TMAX is the maximum time
+    # it's used to create a list that contains the distance of each pair of points and the time
+    # difference between them to try to create fixation points and saccades. DMAX is the maximum
+    # space distance between two points to consider them a fixation; TMAX is the maximum time
     list_distances = list()
     i = 0
     DMAX = 90
     TMAX = 400
     while i + 1 < len(list_circles):
-        num = (list_circles[i+1][0]-list_circles[i][0])**2 + (list_circles[i+1][1]-list_circles[i][1])**2
+        num = (list_circles[i+1][0]-list_circles[i][0])**2 + \
+            (list_circles[i+1][1]-list_circles[i][1])**2
         square = math.sqrt(num)
         time_difference = list_circles[i+1][2]-list_circles[i][2]
-        list_distances.append((round(square, 2), round(time_difference, 2), square < DMAX))
-        i=i+1
+        list_distances.append(
+            (round(square, 2), round(time_difference, 2), square < DMAX))
+        i = i+1
 
-    #this part is used to find the indexes of the final fixation points
+    # this part is used to find the indexes of the final fixation points
     list_fix_temp = list()
     i = 0
     sum_t = list_distances[0][1]
     while i + 1 < len(list_distances):
 
-        #if the distance between two point is already > DMAX and the delta time is > 80 ms
-        # tutti i punti campionati entro 80 ms indifferentemente dalla loro distanza nello 
+        # if the distance between two point is already > DMAX and the delta time is > 80 ms
+        # tutti i punti campionati entro 80 ms indifferentemente dalla loro distanza nello
         # spazio vanno assimilati all'interno di un unico Fixation point
-        # la distanza temporale di campionamento è intorno ai 40 ms, quindi nel caso in cui 
-        # siano di per sé troppo lontani Prendiamo i tre punti successivi e li includiamo 
-        # in un unico Fixation point dato che la distanza temporale di questi tre punti si 
-        # aggira intorno agli 80 ms più o meno qualche millisecondo.la distanza spaziale 
+        # la distanza temporale di campionamento è intorno ai 40 ms, quindi nel caso in cui
+        # siano di per sé troppo lontani Prendiamo i tre punti successivi e li includiamo
+        # in un unico Fixation point dato che la distanza temporale di questi tre punti si
+        # aggira intorno agli 80 ms più o meno qualche millisecondo.la distanza spaziale
         # in questo caso non ci interessa
         if not list_distances[i][2] and i + 3 < len(list_distances):
-            # aggrego i tre punti e non passo l'iterazione successiva ma tre successive perché 
+            # aggrego i tre punti e non passo l'iterazione successiva ma tre successive perché
             # i punti di mezzo li ho già inseriti in un Fixation
             list_fix_temp.append((i, i+1, i+2))
             sum_t = list_distances[i+3][1]
             #print("primo if, t e i sono " + str(i) + ", " + str(i+1) + ", " + str(i+2) + ", "+ str(i+3))
-            i=i+3
+            i = i+3
 
         else:
-            #Negli altri casi invece prendo il primo punto a disposizione e lo utilizzo 
-            # come possibile Fixation point. Vado quindi a determinare la distanza tra questo 
-            # punto e tutti i successivi fino a quando o la distanza degll'iesimo punto è 
-            # maggiore di DMAX, Oppure il tempo di campionamento a superato i 400 ms. 
-            # Successivamente per ogni Fixation point andrò a calcolare la media di tutti 
+            # Negli altri casi invece prendo il primo punto a disposizione e lo utilizzo
+            # come possibile Fixation point. Vado quindi a determinare la distanza tra questo
+            # punto e tutti i successivi fino a quando o la distanza degll'iesimo punto è
+            # maggiore di DMAX, Oppure il tempo di campionamento a superato i 400 ms.
+            # Successivamente per ogni Fixation point andrò a calcolare la media di tutti
             # i punti andando a trovare il punto medio e utilizzando quello come Fixation finale
 
             j = i
             while i + 1 < len(list_distances):
 
-                num = (list_circles[i+1][0]-list_circles[j][0])**2 + (list_circles[i+1][1]-list_circles[j][1])**2
+                num = (list_circles[i+1][0]-list_circles[j][0])**2 + \
+                    (list_circles[i+1][1]-list_circles[j][1])**2
                 square = math.sqrt(num)
                 time_difference = list_circles[i+1][2]-list_circles[j][2]
 
                 if square < DMAX and time_difference <= TMAX:
-                    
+
                     list_fix_temp.append((j, i+1))
                     #print("while, t e i sono " + str(j) + ", " + str(i+1) + ", " + str(i+1))
                     i = i + 1
-                
-                else: 
+
+                else:
                     #print("sono nel while + else")
                     i = i + 1
                     break
 
-    #used to cast the tuples of indexes contained in the previous list into sets of indexes
+    # used to cast the tuples of indexes contained in the previous list into sets of indexes
     list_fix = list()
     i = 0
     for elem in list_fix_temp:
         list_fix.append(set(elem))
 
-    #each set is checked to join the sets that represent the same fixation point
+    # each set is checked to join the sets that represent the same fixation point
     while i + 1 < len(list_fix):
         if len(list_fix[i].intersection(list_fix[i+1])) > 0:
             j = i
@@ -125,9 +127,9 @@ with open("csv/GazeData.csv") as file:
                 list_fix[j] = list_fix[j].union(list_fix[i+1])
                 list_fix.pop(i+1)
         else:
-            i=i+1  
+            i = i+1
 
-    #eventually, with this part, for every set (that represents the points that should be considered 
+    # eventually, with this part, for every set (that represents the points that should be considered
     # as the same fixation point), it's calculated the medium point to create the final fixation point
     list_fix_points = list()
     for fix_set in list_fix:
@@ -157,40 +159,33 @@ with open("csv/GazeData.csv") as file:
         text(cir[0], cir[1], str(i), color="red", fontsize=12)
         i=i+1
     '''
-    
-
-    
-
-
 
     '''
     
     '''
 
-    #setting of the graph 
-    
+    # setting of the graph
+
     """ f = figure()
     f.set_figwidth(16)
     f.set_figheight(9) """
 
-    img = imread("img/Griglia.png")
+    img = imread("img/text.png")
     fig, ax = subplots()
     ax.imshow(img, extent=[0, 2000, 1200, 0])
 
-
-    #shows the fixation points
+    # shows the fixation points
     for center in list_fix_points:
-        scatter(center[0], center[1], s=500, facecolors='none', edgecolors='blue')
+        scatter(center[0], center[1], s=500,
+                facecolors='none', edgecolors='blue')
 
-    #cycle used to enumerate all the circles 
+    # cycle used to enumerate all the circles
     i = 0
     for cir in list_fix_points:
         text(cir[0], cir[1], str(i), color="red", fontsize=12)
-        i=i+1
+        i = i+1
 
-        
-        
-    #shows the saccades
+    # shows the saccades
     list_x = list()
     list_y = list()
     for fix in list_fix_points:
@@ -198,8 +193,7 @@ with open("csv/GazeData.csv") as file:
         list_y.append(fix[1])
     plot(list_x, list_y)
 
-
-    #I want to create the heatmap
+    # I want to create the heatmap
 
     # Create a dataset
 
@@ -209,24 +203,23 @@ with open("csv/GazeData.csv") as file:
         list_heat.append([list_x[ind], list_y[ind]])
         list_indx.append(ind)
 
-
     df = pd.DataFrame(list_heat, columns=['X', 'Y'], index=list_indx)
-    #df.head()
+    # df.head()
 
     # Default heatmap
-    img = imread("img/Griglia.png")
+    img = imread("img/text.png")
     fig, ak = subplots()
     ak.imshow(img)
     list_y_vecchi = list()
     for y in list_y:
         list_y_vecchi.append(1200 - y)
-    hist2d(list_x, list_y_vecchi, bins=30, range=[[0, 2000], [0, 1200]], cmap="Greys")    #icefire, Greys
+    hist2d(list_x, list_y_vecchi, bins=30, range=[
+           [0, 2000], [0, 1200]], cmap="Greys")  # icefire, Greys
     #p1 = sns.heatmap(df)
     #sns.jointplot(x=list_x, y=list_y, kind="kde")
 
-    #funziona ma è da migliorare
+    # funziona ma è da migliorare
 
-    
     '''
     print(*list_circles[:3], sep = "\n")
     print("\n")
@@ -278,7 +271,6 @@ with open("csv/GazeData.csv") as file:
     print(list_distances[:10])
 
     '''
-
 
     show()
     '''savefig('GazePoints.png')'''
